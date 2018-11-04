00218735cebb051ee32add5e2bd63b9ec0f7097e
Michael Robbins <michael.robbins@contino.io>
2018-11-04 17:48:59 +1100

    Updating test data script to py3

--- tests/files/data_files/generate_test_data.py
+++ tests/files/data_files/generate_test_data.py
@@ -1,14 +1,14 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
 import csv
 
-with open("bigdata1.tsv", "wb") as data3_file:
+with open("bigdata1.tsv", "w") as data3_file:
     writer = csv.writer(data3_file, delimiter='\t')
 
     for data in range(111111, 999999, 2):
         writer.writerow([str(data), "abcde", "blah123"])
 
-with open("bigdata2.tsv", "wb") as data3_file:
+with open("bigdata2.tsv", "w") as data3_file:
     writer = csv.writer(data3_file, delimiter='\t')
 
     for data in range(111112, 999999, 2):
